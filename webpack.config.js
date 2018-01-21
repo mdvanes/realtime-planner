3458468ced695ea6a6af8d8e375afe347a4ef34e
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-01-21 19:31:06 +0100

    Add Webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.ts'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, 'bower_components')
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: [
+                    { loader: 'ts-loader' }
+                ]
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
