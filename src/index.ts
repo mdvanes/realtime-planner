ad3b804bfbb68d199003c64908b5e39dbcb1401e
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-01-28 20:37:01 +0100

    TSLint and Prettier for src

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as Rx from 'rxjs/Rx';
+import * as Rx from "rxjs/Rx";
 
 // console.log('ts hi');
 //
@@ -7,29 +7,26 @@ import * as Rx from 'rxjs/Rx';
 //     .map(x => x + '!!!')
 //     .subscribe(x => console.log('Observable of array', x));
 
-const subject = Rx.Observable.webSocket('ws://localhost:9001');
-subject
-    .retry()
-    .subscribe(
-        (msg) => {
-            //const x = JSON.stringify(JSON.parse(msg));
-            console.log('message received: ' + msg, msg);
-            const result:any = msg;
-            console.log(result.foo);
-            render(result);
-        },
-        (err) => console.log(err),
-        () => console.log('complete')
-    );
-subject.next(JSON.stringify({ message: 'Msg from browser' }));
+const subject = Rx.Observable.webSocket("ws://localhost:9001");
+subject.retry().subscribe(
+  msg => {
+    // const x = JSON.stringify(JSON.parse(msg));
+    console.log("message received: " + msg, msg);
+    const result: any = msg;
+    console.log(result.foo);
+    render(result);
+  },
+  err => console.log(err),
+  () => console.log("complete")
+);
+subject.next(JSON.stringify({ message: "Msg from browser" }));
 
 // TODO add typing for "state", i.e. it should have an "appointments" property
 function render(state) {
-    const templ =
-        `<dd>
+  const templ = `<dd>
             <dt>FOO</dt>
             <dl>${state.foo}</dl>
         </dd>`;
-    //document.getElementById('main').innerHTML = templ;
-    document.body.innerHTML = templ;
+  // document.getElementById('main').innerHTML = templ;
+  document.body.innerHTML = templ;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,32 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.ts'),
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-        modules: [
-            path.resolve(__dirname, 'node_modules'),
-            path.resolve(__dirname, 'bower_components')
-        ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: [
-                    { loader: 'ts-loader' }
-                ]
-            }
-        ]
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    },
-    plugins: [new HtmlWebpackPlugin()]
+  entry: path.resolve(__dirname, "src/index.ts"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    modules: [
+      path.resolve(__dirname, "node_modules"),
+      path.resolve(__dirname, "bower_components")
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [{ loader: "ts-loader" }, { loader: "tslint-loader" }]
+      }
+    ]
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  plugins: [new HtmlWebpackPlugin()]
 };
