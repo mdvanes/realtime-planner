57674b2f66a867086fcd7f2901e98a0b159b2f62
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-01-29 19:37:22 +0100

    Render some appointments

---
+++ server/appointments.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    name: 'John'
+  },
+  {
+    name: 'Jack'
+  }
+];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as Rx from 'rxjs/Rx';
+// import {connectableObservableDescriptor} from "rxjs/observable/ConnectableObservable";
 
 // console.log('ts hi');
 //
@@ -11,9 +12,9 @@ const subject = Rx.Observable.webSocket('ws://localhost:9001');
 subject.retry().subscribe(
   msg => {
     // const x = JSON.stringify(JSON.parse(msg));
-    console.log('message received: ' + msg, msg);
+    // console.log('message received: ' + msg, msg);
     const result: any = msg;
-    console.log(result.foo);
+    // console.log(result.foo);
     render(result);
   },
   err => console.log(err),
@@ -23,10 +24,20 @@ subject.next(JSON.stringify({ message: 'Msg from browser' }));
 
 // TODO add typing for "state", i.e. it should have an "appointments" property
 function render(state) {
-  const templ = `<dd>
-            <dt>FOO</dt>
-            <dl>${state.foo}</dl>
-        </dd>`;
+  // const templ = `<dd>
+  //           <dt>FOO</dt>
+  //           <dl>${state[0].name}</dl>
+  //       </dd>`;
+  const output = renderTable(state);
   // document.getElementById('main').innerHTML = templ;
-  document.body.innerHTML = templ;
+  //console.log(output);
+  document.body.innerHTML = output;
+}
+
+function renderTable(apts) {
+  return '<table>' + apts.map(renderRow).join('') + '</table>';
+}
+
+function renderRow(apt) {
+  return `<tr><td>${apt.name}</td></tr>`;
 }
