6e0fe6c5505a32444a895c481f4935a4ff04d792
M.D. van Es <es@mdworld.nl>
2018-01-30 14:48:42 +0100

    Extract appointment creation

---
+++ server/appointment.js
@@ -0,0 +1,95 @@
+function randomizeFirstName() {
+    const names = [
+        'Peter',
+        'Carol',
+        'Bruce',
+        'Donald',
+        'Tony',
+        'Luke',
+        'Natasha',
+        'Matt',
+        'Steve',
+        'James',
+        'Warren',
+        'Steven',
+        'Hank',
+        'Bobby',
+        'Clint',
+        'Jennifer',
+        'Jessica'
+    ];
+    return names[Math.floor(Math.random() * names.length)];
+}
+
+function randomizeLastName() {
+    const names = [
+        'Parker',
+        'Danvers',
+        'Banner',
+        'Blake',
+        'Stark',
+        'Cage',
+        'Romanova',
+        'Murdock',
+        'Rogers',
+        'Howlett',
+        'Worthington',
+        'Strange',
+        'Pym',
+        'Drake',
+        'Barton',
+        'Walters',
+        'Jones'
+    ];
+    return names[Math.floor(Math.random() * names.length)];
+}
+
+function maybeGetPhonenumber() {
+    if (Math.random() > 0.5) {
+        return `06${Math.random()
+            .toString()
+            .slice(2, 10)}`;
+    }
+    return null;
+}
+
+function guessEmail(firstName, lastName) {
+    return (
+        firstName.slice(0, 1) +
+        '.' +
+        lastName +
+        '@example.com'
+    ).toLowerCase();
+}
+
+/*
+random date
+
+const date = new Date();
+date.setHours(0);
+// Random day 3 to 300 days in the future
+const futureOffset = Math.floor(Math.random() * 297) + 3;
+date.setDate( date.getDate() + futureOffset);
+const newMonth = (date.getMonth() + 1).toString().padStart(2, '0');
+const newDay = date.getDate().toString().padStart(2, '0');
+this.dispatch(ING.ACTIONS.APPOINTMENT.DATE_CHANGED(`${date.getFullYear()}-${newMonth}-${newDay}`));
+const times = ['1000', '1100', '1300', '1400'];
+const randomTime = times[Math.floor(Math.random() * times.length)];
+ */
+
+module.exports = class Appointment {
+    constructor(firstName, lastName, phone, email, date) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.date = date;
+    }
+
+    static createRandom() {
+        console.log('a random appointment');
+        const firstName = randomizeFirstName();
+        const lastName = randomizeLastName();
+        return new Appointment(firstName, lastName, maybeGetPhonenumber(), guessEmail(firstName, lastName), new Date());
+    }
+}
--- server/ws-server.js
+++ server/ws-server.js
@@ -4,6 +4,7 @@ const server = require('http').createServer();
 //const wss = new WebSocket.Server({ port: 9001, path: 'realtime', origin: 'http://localhost:9000' });
 const wss = new WebSocket.Server({ server });
 const appointments = require('./appointments');
+const appointment = require('./appointment');
 
 // Also mount the app here
 server.on('request', app);
@@ -13,7 +14,7 @@ wss.on('connection', function connection(ws) {
     console.log('received: %s', message);
     const msg = JSON.parse(message);
     if (msg.message === 'add') {
-      appointments.push(randomAppointment());
+      appointments.push(appointment.createRandom());
       wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
           client.send(JSON.stringify(appointments));
@@ -36,99 +37,6 @@ wss.on('connection', function connection(ws) {
   randomAdd(ws);
 });
 
-function randomizeFirstName() {
-  const names = [
-    'Peter',
-    'Carol',
-    'Bruce',
-    'Donald',
-    'Tony',
-    'Luke',
-    'Natasha',
-    'Matt',
-    'Steve',
-    'James',
-    'Warren',
-    'Steven',
-    'Hank',
-    'Bobby',
-    'Clint',
-    'Jennifer',
-    'Jessica'
-  ];
-  return names[Math.floor(Math.random() * names.length)];
-}
-
-function randomizeLastName() {
-  const names = [
-    'Parker',
-    'Danvers',
-    'Banner',
-    'Blake',
-    'Stark',
-    'Cage',
-    'Romanova',
-    'Murdock',
-    'Rogers',
-    'Howlett',
-    'Worthington',
-    'Strange',
-    'Pym',
-    'Drake',
-    'Barton',
-    'Walters',
-    'Jones'
-  ];
-  return names[Math.floor(Math.random() * names.length)];
-}
-
-function maybeGetPhonenumber() {
-  if (Math.random() > 0.5) {
-    return `06${Math.random()
-      .toString()
-      .slice(2, 10)}`;
-  }
-  return null;
-}
-
-function guessEmail(firstName, lastName) {
-  return (
-    firstName.slice(0, 1) +
-    '.' +
-    lastName +
-    '@example.com'
-  ).toLowerCase();
-}
-
-/*
-
-random date
-
-const date = new Date();
-                    date.setHours(0);
-                    // Random day 3 to 300 days in the future
-                    const futureOffset = Math.floor(Math.random() * 297) + 3;
-                    date.setDate( date.getDate() + futureOffset);
-                    const newMonth = (date.getMonth() + 1).toString().padStart(2, '0');
-                    const newDay = date.getDate().toString().padStart(2, '0');
-                    this.dispatch(ING.ACTIONS.APPOINTMENT.DATE_CHANGED(`${date.getFullYear()}-${newMonth}-${newDay}`));
-                    const times = ['1000', '1100', '1300', '1400'];
-                    const randomTime = times[Math.floor(Math.random() * times.length)];
-
- */
-
-function randomAppointment() {
-  const firstName = randomizeFirstName();
-  const lastName = randomizeLastName();
-  return {
-    firstName,
-    lastName,
-    date: new Date(),
-    phone: maybeGetPhonenumber(),
-    email: guessEmail(firstName, lastName)
-  };
-}
-
 function randomAdd(ws) {
   // const minMs = 1000;
   // const maxMs = 30000;
@@ -139,9 +47,11 @@ function randomAdd(ws) {
   //   randomAdd(ws);
   // }, timeout);
 
-  appointments.push(randomAppointment());
+    // TODO observe changes to appointments and do send
+
+  appointments.push(appointment.createRandom());
   ws.send(JSON.stringify(appointments));
-  appointments.push(randomAppointment());
+  appointments.push(appointment.createRandom());
   ws.send(JSON.stringify(appointments));
 }
 
