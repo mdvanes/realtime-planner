6c7bbe14d2a2afd1cd82d7d7fd687a570cf6e49a
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-02-03 09:25:45 +0100

    Add auto button

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,11 @@ function observeAppointmentsChanges() {
   subject.retry().subscribe(
     msg => {
       // const x = JSON.stringify(JSON.parse(msg));
-      // console.log('message received: ' + msg, msg);
+      console.log('message received: ' + msg, msg);
       const result: any = msg;
 
       // TODO not result.length > 0, but updates > 0 (msg pushed does not mean appointments added)
-      if (!document.hasFocus() && result.length > 0) {
+      if (!document.hasFocus() && result.appointments && result.appointments.length > 0) {
         // TODO it is probably bad practice to set this in two places (also in observeWindowFocus)
         document.title = notificationTitle.addAndGetTitle();
       } else {
@@ -29,17 +29,24 @@ function observeAppointmentsChanges() {
     err => console.log(err),
     () => console.log('complete')
   );
-  subject.next(JSON.stringify({ message: 'Msg from browser' }));
+  //subject.next(JSON.stringify({ message: 'Msg from browser' }));
+
+  // TODO automatically re-connect web socket if server restarts and get state - https://gearheart.io/blog/auto-websocket-reconnection-with-rxjs/
 
   // TODO add typing for "state", i.e. it should have an "appointments" property
   function render(state) {
-    const output = renderButton() + '<br/>' + renderTable(state);
+    const output = renderButton() + ' ' + renderToggle(state.auto) + '<div class="mdl-layout-spacer"></div><br/>' + renderTable(state.appointments);
     document.getElementById('target').innerHTML = output;
     document.getElementById('button-add').addEventListener('click', add);
+    document.getElementById('button-auto').addEventListener('click', autoToggle);
   }
 
   function add() {
     subject.next(JSON.stringify({ message: 'add' }));
+  }
+
+  function autoToggle() {
+    subject.next(JSON.stringify({ message: 'auto' }));
   }
 }
 observeAppointmentsChanges();
@@ -57,8 +64,22 @@ observeWindowFocus();
 
 function renderButton() {
   return `<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" id="button-add">
-    <i class="material-icons">add</i>
-    </button><div class="mdl-layout-spacer"></div>`;
+        <i class="material-icons">add</i>
+    </button>`;
+}
+
+function renderToggle(isAuto: boolean) {
+  // return `<label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-1">
+  //   <input type="checkbox" id="switch-1" class="mdl-switch__input" checked>
+  //   <span class="mdl-switch__label"></span>
+  // </label>`;
+  let classNames = 'mdl-button mdl-js-button mdl-button--raised';
+  if(isAuto) {
+    classNames += ' mdl-button--colored';
+  }
+  return `<button class="${classNames}" id="button-auto">
+        auto ${isAuto}
+    </button>`;
 }
 
 function renderTable(apts) {
