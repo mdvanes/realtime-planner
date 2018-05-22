aa1a09f1c16fb995b7c1e434e12a153278ba64ce
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-05-22 14:08:06 +0200

    Formatting

--- src/LatestTweet.ts
+++ src/LatestTweet.ts
@@ -1,36 +1,36 @@
 import { bind } from 'hyperhtml/esm';
 
-// TODO: bind this.state.lastTweet (or something) to my-last-tweet.tweet-info wsStream.ts after construction
-// TODO: convert to TS
 class LatestTweet extends HTMLElement {
+    private html: any;
+
     static get observedAttributes() {return ['tweet-info']; }
 
-    constructor(...args) {
-        super(...args);
+    constructor() {
+        super();
         // console.log('constructing LatestTweet');
         this.html = bind(this);
     }
 
-    connectedCallback() {
+    private connectedCallback() {
         // console.log('tweet-info', this.getAttribute('tweet-info'));
         this.render();
     }
 
-    attributeChangedCallback(name, oldValue, newValue) {
+    private attributeChangedCallback(name, oldValue, newValue) {
         // console.log('Attribute', name, ' changed to', newValue);
-        if(newValue !== oldValue) {
-            //updateStyle(this);
+        if (newValue !== oldValue) {
             this.render();
         }
       }
 
-    render() {
+    private render() {
         try {
             const {text, username, id_str} = JSON.parse(this.getAttribute('tweet-info'));
-            // Hyper does not like "partial attributes", e.g. href="https://twitter.com/x/status/${state.lastTweet.id_str}">
-            const href = 'https://twitter.com/x/status/'+ id_str;
+            // Hyper does not like "partial attributes", e.g.
+            // href="https://twitter.com/x/status/${state.lastTweet.id_str}">
+            const href = 'https://twitter.com/x/status/' + id_str;
             return this.html`THIS JUST IN: <a href="${href}">${text}</a> by ${username}`;
-        } catch(ex) {
+        } catch (ex) {
             return this.html`THIS JUST IN: _`;
         }
     }
